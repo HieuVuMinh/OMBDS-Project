2eafabe2b202b537e99164a76a83edb69be8afe3
thaond1205 <thaond1205@gmail.com>
2021-10-13 11:34:22 +0700

    thao

--- BDS-APIGateway/src/main/java/com/example/bdsapigateway/controller/AgentController.java
+++ BDS-APIGateway/src/main/java/com/example/bdsapigateway/controller/AgentController.java
@@ -3,6 +3,10 @@ package com.example.bdsapigateway.controller;
 import com.example.bdsapigateway.modelDTO.Agent;
 import com.example.bdsapigateway.modelDTO.ResponseObject;
 import com.example.bdsapigateway.service.agent.AgentGatewayService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +19,21 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api-gateway")
+@Api(value="onlinestore", description="API Agent")
 public class AgentController {
 
     @Autowired
     private AgentGatewayService agentGatewayService;
 
+
+    @ApiOperation(value = "Lấy ra tất cả Agent",response = Agent.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Trả ra dữ liệu thành công!"),
+            @ApiResponse(code = 401, message = "Xác thực thất bại!"),
+            @ApiResponse(code = 403, message = "Cố tình truy cập trái phép!"),
+            @ApiResponse(code = 404, message = "Không tồn tại!")
+    }
+    )
     @GetMapping("agents")
     private Flux<Agent> findAllCustomer(){
 //        Flux<Agent> listAgent = agentGatewayService.findAllAgent();
@@ -34,6 +48,15 @@ public class AgentController {
         return agentGatewayService.findAllAgent();
     }
 
+
+    @ApiOperation(value = "Tìm Agent theo id",response = Agent.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Trả ra dữ liệu thành công!"),
+            @ApiResponse(code = 401, message = "Xác thực thất bại!"),
+            @ApiResponse(code = 403, message = "Cố tình truy cập trái phép!"),
+            @ApiResponse(code = 404, message = "Không tồn tại!")
+    }
+    )
     @GetMapping("agents/name/{id}")
     public ResponseEntity<?> findByLastName(@PathVariable("id") Long id) {
         Agent agent = agentGatewayService.findByAgentId(id).block();
